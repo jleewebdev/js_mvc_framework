72ab54b49391aaffc80ff8912280c0da0dbcfbb4
jleewebdev <lee45276@gmail.com>
2016-02-14 17:48:18 +0800

    Add Model methods, triggerChange, addCallback, remove

--- javascripts/framework.js
+++ javascripts/framework.js
@@ -1,21 +1,37 @@
 function ModelConstructor(options) {
   var id_count = 0;
 
-  function Model() {
+  function Model(attrs) {
     id_count++;
     var self = this;
-    self.attributes = {};
+    self.attributes = attrs || {};
     self.id = id_count;
     self.attributes.id = id_count;
+    if (options && options.change && _.isFunction(options.change)) {
+      this.__events.push(options.change);
+    }
   }
 
   Model.prototype = {
+    __events: [],
+
     set: function(key, value) {
       this.attributes[key] = value;
+      this.triggerChange();
     },
 
     get: function(key) {
       return this.attributes[key];
+    },
+
+    triggerChange: function() {
+      this.__events.forEach(function(cb) {
+        cb();
+      });
+    },
+
+    addCallback: function(cb) {
+      this.__events.push(cb);
     }
   };
 
